f99e7f4c999e34132e2eb3afeed48e88c19bd1d6
Benjamine Paul Hanna <bhanna@ascensionacademy.org>
2026-02-10 09:21:33 -0600

    Intial Commit

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.backgroundPlain)
+            
+            VStack {
+                Spacer()
+                Image(.logo)
+                Spacer()
+                HStack {
+                    Image(.card2)
+                    Image(.card3)
+                }
+                Spacer()
+                Image(.button)
+                Spacer()
+                
+                //Score()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .padding(.bottom, 40.0)
+                        
+                    }
+                    Spacer()
+                    VStack {
+                        Text("Computer")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .padding(.bottom, 40.0)
+
+                    }
+                    Spacer()
+                }
+                .foregroundStyle(.white)
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
